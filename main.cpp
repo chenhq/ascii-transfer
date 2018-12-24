25c91e6d695d8819b99b59bb7bcab3c7fb3e0daf
shimniok@gmail.com <shimniok@gmail.com>
2018-12-24 12:27:29 -0700

    added -r -s flags for send/recv

--- main.cpp
+++ main.cpp
@@ -5,8 +5,35 @@
 
 AsciiTransfer t;
 
+void usage() {
+    puts("usage: axfer -r");
+    puts("       axfer -s file");
+}
+
 int main(int argc, char **argv) {
     char file[1024];
+    int status = 0;
 
-    t.recv();
+    if (argc >= 2) {
+        if (!strcmp("-r", argv[1])) {
+            if (argc == 2) {
+                t.recv();
+            } else {
+                status = -1;
+                usage();
+            }
+        } else if (!strcmp("-s", argv[1])) {
+            if (argc == 3) {
+                t.send(argv[2]);
+            } else {
+                status = -1;
+                usage();
+            }
+        }
+    } else {
+        status = -1;
+        usage();
+    }
+
+    return status;
 }
